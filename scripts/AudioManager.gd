b3d052f2c2e9d0206928d1553798ecc5d50fb0b2
CharlesVazetta <charles.vanzetta@gmail.com>
2025-08-12 21:42:30 +0200

    feat: add ambience loop and pause menu (esc)

---
+++ scripts/AudioManager.gd
@@ -0,0 +1,11 @@
+extends Node
+class_name AudioManager
+
+@onready var daynight: DayNight = $"../DayNight"
+@onready var amb: AudioStreamPlayer = $"../Ambience"
+
+func _process(_delta: float) -> void:
+	if !amb: return
+	var night := daynight.time_of_day < 6.0 or daynight.time_of_day > 20.0
+	amb.volume_db = night ? -8.0 : -12.0
+	if !amb.playing: amb.play()
