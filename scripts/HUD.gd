11d7cf4f9f674ead6d4333a9fe95aca9ab64cfc1
CharlesVazetta <charles.vanzetta@gmail.com>
2025-08-12 21:28:26 +0200

    ajout hud

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -12,22 +12,22 @@ var stats: Stats
 var water_system: WaterSystem
 
 func _ready() -> void:
-    stats = get_tree().get_first_node_in_group("stats") as Stats
-    water_system = get_node("../WaterSystem") as WaterSystem
-    if btn_filter:
-        btn_filter.pressed.connect(_on_filter_pressed)
+	stats = get_tree().get_first_node_in_group("stats") as Stats
+	water_system = get_node("../WaterSystem") as WaterSystem
+	if btn_filter:
+		btn_filter.pressed.connect(_on_filter_pressed)
 
 func _process(_delta: float) -> void:
-    if stats == null: return
-    pb_hunger.value = stats.hunger
-    pb_thirst.value = stats.thirst
-    pb_energy.value = stats.energy
-    pb_temp.value = clamp((stats.body_temp - 34.0) * 33.3, 0.0, 100.0) # 34-37°C
-    
-    if water_system:
-        lbl_water.text = "Eau: %.1fL brute / %.1fL propre" % [water_system.water_raw, water_system.water_clean]
-        btn_filter.disabled = !water_system.can_filter_1l()
+	if stats == null: return
+	pb_hunger.value = stats.hunger
+	pb_thirst.value = stats.thirst
+	pb_energy.value = stats.energy
+	pb_temp.value = clamp((stats.body_temp - 34.0) * 33.3, 0.0, 100.0) # 34-37°C
+	
+	if water_system:
+		lbl_water.text = "Eau: %.1fL brute / %.1fL propre" % [water_system.water_raw, water_system.water_clean]
+		btn_filter.disabled = !water_system.can_filter_1l()
 
 func _on_filter_pressed() -> void:
-    if water_system and water_system.filter_1l():
-        print("1L d'eau filtrée !")
+	if water_system and water_system.filter_1l():
+		print("1L d'eau filtrée !")
